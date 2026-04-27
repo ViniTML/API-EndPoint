28231e79be3b92e170303a8c701acb5fbbf9415d
ViniTML <vinicius_tml@hotmail.com>
2026-04-27 20:02:08 -0300

    Commit principal

---
+++ AulaApiDevOpsManhaPiaget/Data/AulaApiDevOpsManhaPiagetContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AulaApiDevOpsManhaPiaget.Models;
+
+namespace AulaApiDevOpsManhaPiaget.Data
+{
+    public class AulaApiDevOpsManhaPiagetContext : DbContext
+    {
+        public AulaApiDevOpsManhaPiagetContext (DbContextOptions<AulaApiDevOpsManhaPiagetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produtos> Produtos { get; set; } = default!;
+        public DbSet<AulaApiDevOpsManhaPiaget.Models.Categoria> Categoria { get; set; } = default!;
+    }
+}
---
+++ AulaApiDevOpsManhaPiaget/Migrations/20260317000502_cat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AulaApiDevOpsManhaPiaget.Migrations
+{
+    /// <inheritdoc />
+    public partial class cat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categoria");
+        }
+    }
+}
